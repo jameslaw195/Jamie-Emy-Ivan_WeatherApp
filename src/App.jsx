32f8817dae63b42c2d0215c1090c21dc58cb9b74
Elmira Jamalian <51411715+emyjamalian@users.noreply.github.com>
2023-12-04 18:44:08 +0100

    adding header and a filtered good weather activities

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
 import "./App.css";
 import Form from "./components/Form";
 import List from "./components/List";
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const isGoodWeather = true;
+  const [activities, setActivities] = useLocalStorageState("activities", {
+    defaultValue: [],
+  });
 
   function handleAddActivity(newActivity) {
     setActivities([...activities, newActivity]);
   }
 
+  const goodWeatherActivities = activities.filter((activity) => {
+    return activity.isForGoodWeather === isGoodWeather;
+  });
+
   return (
     <>
-      <List viewList={activities}></List>
+      {/* <List viewList={activities}></List> */}
+      <List viewList={goodWeatherActivities}></List>
+
       <Form onAddActivity={handleAddActivity}></Form>
     </>
   );
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,6 +1,8 @@
 export default function List({ viewList }) {
   return (
     <ul>
+      <h3>{"heading"}</h3>
+
       {viewList.map((activity) => (
         <li key={activity.id}>{activity.name}</li>
       ))}
